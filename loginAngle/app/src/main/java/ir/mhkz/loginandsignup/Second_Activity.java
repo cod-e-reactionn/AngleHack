69c10c106f22e0aaaf94f782dc9b47a7a9ef03ea
Karanb786 <karanbagle786@icloud.com>
2018-07-01 15:53:30 +0530

    Intent

---
+++ loginAngle/app/src/main/java/ir/mhkz/loginandsignup/Second_Activity.java
@@ -0,0 +1,33 @@
+package ir.mhkz.loginandsignup;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Second_Activity extends AppCompatActivity {
+
+ImageView adddd,pro;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_);
+     adddd=findViewById(R.id.addd);
+     pro=findViewById(R.id.pram);
+        adddd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(Second_Activity.this,advertisment.class);
+                startActivity(intent);
+            }
+
+
+        });
+
+        }
+
+
+
+    }
+
