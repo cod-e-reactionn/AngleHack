30e347ed86d8eb3a2d1efc878c29393e0629865b
Karanb786 <karanbagle786@icloud.com>
2018-08-21 16:27:54 +0530

    New Beginning

--- loginAngle/app/src/main/java/ir/mhkz/loginandsignup/setupActivity.java
+++ loginAngle/app/src/main/java/ir/mhkz/loginandsignup/setupActivity.java
@@ -2,6 +2,7 @@ package ir.mhkz.loginandsignup;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -12,12 +13,20 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class setupActivity extends AppCompatActivity {
 
@@ -26,8 +35,12 @@ public class setupActivity extends AppCompatActivity {
     DatabaseReference userref;
     FirebaseAuth firebaseAuth;
     String currentUserId;
+    private StorageReference UserProfileImage;
+
     private ProgressDialog loadingbar;
     private ProgressDialog progressDialog;
+    CircleImageView profileImg;
+    final static int Gallery_pick=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,11 +51,13 @@ public class setupActivity extends AppCompatActivity {
         email_add= findViewById(R.id.email_add);
         contact_number=findViewById(R.id.email_add);
         submit_info = findViewById(R.id.submit_info);
+        profileImg=findViewById(R.id.profile_img);
         progressDialog = new ProgressDialog(this);
         loadingbar = new ProgressDialog(this);
        firebaseAuth= FirebaseAuth.getInstance();
        currentUserId=firebaseAuth.getCurrentUser().getUid();
        userref= FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
+       UserProfileImage= FirebaseStorage.getInstance().getReference().child("profile Images");
 
         submit_info.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,8 +69,58 @@ public class setupActivity extends AppCompatActivity {
 
             }
         });
+        profileImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+               startActivityForResult(galleryIntent,Gallery_pick);
+            }
+        });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==Gallery_pick && resultCode==RESULT_OK && data !=null)
+        {
+            Uri imageuri= data.getData();
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1,1)
+                    .start(this);
+        }
+
+        if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE)
+        {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (requestCode==RESULT_OK)
+            {
+                Uri resultUri = result.getUri();
+                final StorageReference filePath = UserProfileImage.child(currentUserId+ ".jpg");
+
+                filePath.putFile(resultUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                final Uri downloadUrl=uri;
+
+                            }
+                        });
+                    }
+                });
+                        }
+                    }
+
+
+            }
+
+
 
     private void SaveAccountInfo() {
         String fname = first_name.getText().toString();
